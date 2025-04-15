24e260551608bafceffd4bc8f0cefc33fe04b9b8
Nils Hausfeld <nils.hausfeld@gmx.de>
2025-04-15 11:15:19 +0200

    [WIN] Guarantee that only one instance is running

--- GenHub/GenHub.Windows/Program.cs
+++ GenHub/GenHub.Windows/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Avalonia;
@@ -7,12 +9,28 @@ namespace GenHub.Windows;
 
 class Program
 {
+    private static Mutex s_Mutex;
+    private const string MutexName = "Global\\GenHub";
+    private const int SW_RESTORE = 9; // Windows API constant to restore a window
+
+    [DllImport("user32.dll")]
+    private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
     public static void Main(string[] args)
     {
+        if (IsAnotherInstanceRunning())
+        {
+            FocusRunningInstance();
+            return;
+        }
+
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
@@ -22,4 +40,22 @@ class Program
             .UsePlatformDetect()
             .WithInterFont()
             .LogToTrace();
+
+    private static bool IsAnotherInstanceRunning()
+    {
+        s_Mutex = new Mutex(true, MutexName, out bool createdNew);
+        return !createdNew;
+    }
+
+    private static void FocusRunningInstance()
+    {
+        var process = Process.GetProcessesByName(Process.GetCurrentProcess().ProcessName).FirstOrDefault();
+
+        if(process == null)
+            return;
+
+        var windowHandle = process.MainWindowHandle;
+        ShowWindow(windowHandle, SW_RESTORE);
+        SetForegroundWindow(windowHandle);
+    }
 }
